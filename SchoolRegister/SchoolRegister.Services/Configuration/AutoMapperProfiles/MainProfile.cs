650f4a7aa88284441eebf4dde21dc967fae647fb
Janson2505 <jan.pijanka@onet.pl>
2025-05-15 17:11:42 +0200

    lab6

---
+++ SchoolRegister/SchoolRegister.Services/Configuration/AutoMapperProfiles/MainProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.ViewModels.VM;
+using System.Text.RegularExpressions;
+namespace SchoolRegister.Services.Configuration.AutoMapperProfiles;
+public class MainProfile : Profile
+{
+    public MainProfile()
+    {
+       
+    }
+}
