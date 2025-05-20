e725a521f8f42a23027df2620b9a9284884f2f62
Janson2505 <jan.pijanka@onet.pl>
2025-05-20 20:19:35 +0200

    lab6

---
+++ SchoolRegister/SchoolRegister.ViewModels/VM/JwtOptionsVm.cs
@@ -0,0 +1,10 @@
+﻿namespace SchoolRegister.ViewModels.VM
+{
+    public class JwtOptionsVm
+    {
+        public string Issuer { get; set; } = null!;
+        public string Audience { get; set; } = null!;
+        public string SecretKey { get; set; } = null!;
+        public int TokenExpirationMinutes { get; set; }
+    }
+}
---
+++ SchoolRegister/SchoolRegister.ViewModels/VM/SendEmailToParentVm.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class SendEmailToParentVm
+    {
+        [Required]
+        public int SenderId { get; set; }
+        [Required]
+        public int StudentId { get; set; }
+        [Required]
+        public string Title { get; set; } = null!;
+        [Required]
+        public string Content { get; set; } = null!;
+    }
+}
